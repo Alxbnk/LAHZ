2f635184932252da1267adcd860e977931108217
Alxbnk <Alxbnk@hotmail.com>
2018-02-27 11:16:36 +0100

    Test

--- Generator.java
+++ Generator.java
@@ -10,7 +10,7 @@ public class Generator
          int m = (int) (Math.random() * 9 + 1);
          int t = (int) (Math.random() * 9 + 1);
          System.out.println("Im the Rabbit and im standing on: " + x + ", " + m);
-         System.out.println("Im the Snake and im standing on: " + y + ", " + t + "\n");
+         System.out.println("Im the Snake and im standing on: " + y + ", " + l + "\n");
        
          if(((x + 1) == y || (x - 1) == y) && ((m + 1) == t || (m - 1) == t))
          {
