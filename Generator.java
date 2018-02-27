4aac0601f1a3da1e662dc5d8ca555a595ea9fa64
mathias <mathias_honberg@hotmail.com>
2018-02-27 10:42:04 +0100

    LAHZ

--- Generator.java
+++ Generator.java
@@ -3,27 +3,29 @@ public class Generator
    
    public void move()
    {
-      while(true)
+     while(true)
       {
          int x = (int) (Math.random() * 9 + 1);
          int y = (int) (Math.random() * 9 + 1);
-         System.out.println("Im the Rabbit and im standing on: " + x + ", " + x);
-         System.out.println("Im the Snake and im standing on: " + y + ", " + y);
-         if((x + 1) == y || (x - 1) == y)
+         int m = (int) (Math.random() * 9 + 1);
+         int t = (int) (Math.random() * 9 + 1);
+         System.out.println("Im the Rabbit and im standing on: " + x + ", " + m);
+         System.out.println("Im the Snake and im standing on: " + y + ", " + t + "\n");
+       
+         if(((x + 1) == y || (x - 1) == y) && ((m + 1) == t || (m - 1) == t))
          {
-         System.out.println("Rabbit: Nooo please dont eat me!");
+         System.out.println("Rabbit: The Snake is getting close, get me out of here!");
+         
          }
-         if(x != y){
-         System.out.println("Moving around again");
-         }
-         if(x == y){
+         
+         if(x == y && m == t){
          
             break;
          }
       
       
       }
-   
+      System.out.println("Rabbit: Noooo, please dont eat me!");
       System.out.println("Snake: Ahhrrrr im going to eat you ha ha!");
    }
    
