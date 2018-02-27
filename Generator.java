a03cbb00e0f3ff503567507507a2e8c72abf8ce5
mathias <mathias_honberg@hotmail.com>
2018-02-27 11:07:20 +0100

    TOBIAS

--- Generator.java
+++ Generator.java
@@ -1,9 +1,9 @@
 public class Generator 
 {
-   
+ int a = 1;
    public void move()
    {
-     while(true)
+     while(a <= 100)
       {
          int x = (int) (Math.random() * 9 + 1);
          int y = (int) (Math.random() * 9 + 1);
@@ -14,23 +14,27 @@ public class Generator
        
          if(((x + 1) == y || (x - 1) == y) && ((m + 1) == t || (m - 1) == t))
          {
-         System.out.println("Rabbit: The Snake is getting close, get me out of here!");
+         System.out.println("Rabbit: The Snake is getting close, get me out of here!\n");
          
          }
-         
+         a++;
          if(x == y && m == t){
          
+         System.out.println("Rabbit: Noooo, please dont eat me!");
+         System.out.println("Snake: Ahhrrrr im going to eat you ha ha!");
+         
             break;
+            
+
          }
-      
+         
+         
       
       }
-      System.out.println("Rabbit: Noooo, please dont eat me!");
-      System.out.println("Snake: Ahhrrrr im going to eat you ha ha!");
+         System.out.println("If you played as the Snake its GAME OVER!");
+         }
+   
    }
-   
-   
 
 
 
-}
