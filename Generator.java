70afb970f78186301983274fdcd97e32dedfccf4
Alxbnk <Alxbnk@hotmail.com>
2018-02-27 10:39:38 +0100

    LAHZ

--- Generator.java
+++ Generator.java
@@ -7,16 +7,18 @@ public class Generator
       {
          int x = (int) (Math.random() * 9 + 1);
          int y = (int) (Math.random() * 9 + 1);
-         System.out.println("Im the Rabbit and im standing on: " + x + ", " + x);
-         System.out.println("Im the Snake and im standing on: " + y + ", " + y);
-         if((x + 1) == y || (x - 1) == y)
+         int k = (int) (Math.random() * 9 + 1);
+         int l = (int) (Math.random() * 9 + 1);
+         System.out.println("Im the Rabbit and im standing on: " + x + ", " + y + "\n");
+         System.out.println("Im the Snake and im standing on: " + k + ", " + l + "\n");
+         if(((x + 1) == k || (x - 1) == k) && ((y + 1) == l || (y - 1) == l))
          {
          System.out.println("Rabbit: Nooo please dont eat me!");
          }
-         if(x != y){
+         if((x != k) && (y != l)){
          System.out.println("Moving around again");
          }
-         if(x == y){
+         if((x == k) && (y == l)){
          
             break;
          }
